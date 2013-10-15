a1803b63d421423ac09985c7ec2b8ccbf4186f42
Tyrell Keene <s4v1orphoenix@gmail.com>
2013-10-15 17:50:45 -0600

    First commit

---
+++ pdb.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <sys/stat.h>
+#include <sys/mman.h> 
+#include <sys/types.h>
+
+#include "pdb.h"
+
+field_t *alloc_field(const char *field_entry, field_type_t type, table_t *t)
+{
+    field_t *pntr;
+    if(field_entry == NULL){
+        fprintf(stderr, "Refusing to add NULL entry\n");
+        return NULL;
+    }
+    pntr = malloc(sizeof(field_t));
+    pntr->field_entry = malloc(strlen(field_entry) + 1);
+    strncpy(pntr->field_entry, field_entry, strlen(field_entry) + 1);
+    pntr->field_name = (char *) field_names[type];
+    pntr->type = type;
+    pntr->next = NULL;
+    pntr->field_id = t->field_count++;
+    pntr->parent_table = t->table_id;
+    fprintf(stdout, "[Table %u]: Allocated field %u with name:entry '%s: %s'\n", 
+            t->table_id, pntr->field_id, pntr->field_name, pntr->field_entry);
+    return pntr;
+}
+
+int free_field(field_t *pntr)
+{
+    if(pntr->field_entry != NULL)
+        free(pntr->field_entry);
+    free(pntr);
+    return 1;
+}
+
+void free_table(table_t *t)
+{
+    field_t **curr;
+    field_t *field;
+    fprintf(stdout, "Freeing table %u\n", t->table_id);
+    if(t->fields != NULL)
+        for(curr = &t->fields; *curr != NULL;){
+            field = *curr;
+            *curr = field->next;
+            fprintf(stdout, "[Table %u]: Freeing field entry %u\n", t->table_id, 
+                    field->field_id);
+            free_field(field);
+        }
+    free(t);
+    return;
+}
+
+table_t *alloc_table(unsigned short id)
+{
+    table_t *pntr;
+    pntr = malloc(sizeof(table_t));
+    pntr->table_id = id; 
+    pntr->field_count = 0;
+    pntr->fields = NULL;
+    pntr->next = NULL;
+    fprintf(stdout, "Allocated table %u\n", id);
+    return pntr;
+}
+
+int parse_input_file(const char *file_path, table_t *t)
+{
+    int input_fd;
+    struct stat st;
+    off_t file_size = 0;
+    char *data_pntr;
+    char *buffer;
+    char *token;
+    char *string;
+
+    if((input_fd = open(file_path, O_RDONLY)) == -1){
+        perror("open");
+        return -1;
+    }
+    if(fstat(input_fd, &st) < 0){
+        perror("stat");
+        return -1;
+    }
+    file_size = st.st_size;
+    fprintf(stdout, "File size: %jd\n", file_size);
+    if((data_pntr = mmap(NULL, file_size, PROT_READ, 
+                    MAP_PRIVATE | MAP_POPULATE, input_fd, 0)) == MAP_FAILED){
+        perror("mmap");
+        return -1;
+    }
+    if(data_pntr == NULL){
+        perror("mmap");
+        return -1;
+    }
+
+    buffer = malloc(file_size);
+    memcpy(buffer, data_pntr, file_size);
+    munmap(data_pntr, file_size);
+
+    /*TODO:get string, get tokens, put tokens into fields*/
+
+    free(buffer);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    table_t *table;
+    if(parse_input_file(argv[1], table) == -1)
+        return -1;
+    return 0;
+}
---
+++ pdb.h
@@ -0,0 +1,69 @@
+#ifndef PDB_H
+#define PDB_H
+
+const char *field_names[] = {
+    "Gender",
+    "Name First",
+    "Initial Mid",
+    "Name Last",
+    "City",
+    "State",
+    "Country",
+    "Email",
+    "Username",
+    "Password",
+    "Telephone",
+    "Date of Birth",
+    "Occupation",
+    "Company",
+    "Blood Type",
+    "Weight",
+    "Height",
+    "Credit Card Type",
+    "Credit Card Number",
+    "Credit Card CCV2",
+    "Credit Card Expiration Date",
+    NULL,
+};
+
+typedef enum field_type_t{
+    GENDER,
+    NAME_FIRST,
+    INITIAL_MID,
+    NAME_LAST,
+    CITY,
+    STATE,
+    COUNTRY,
+    EMAIL,
+    USERNAME,
+    PASSWORD,
+    TELEPHONE,
+    BIRTHDAY,
+    OCCUPATION,
+    COMPANY,
+    BLOOD_TYPE,
+    WEIGHT,
+    HEIGHT,
+    CC_TYPE,
+    CC_NUM,
+    CC_CVV2,
+    CC_EXP
+}field_type_t;
+
+typedef struct field_t{
+    field_type_t type;
+    char *field_name;
+    char *field_entry;
+    unsigned short field_id;
+    unsigned short parent_table;
+    struct field_t *next;
+}field_t;
+
+typedef struct table_t{
+    field_t *fields;
+    unsigned short table_id;
+    unsigned short field_count;
+    struct table_t *next;
+}table_t;
+
+#endif
